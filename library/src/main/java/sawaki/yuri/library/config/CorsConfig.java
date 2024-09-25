e9082bc11aeeb121d81744b38b74f4c0e43bc2dd
yurisawaki <141770852+yurisawaki@users.noreply.github.com>
2024-09-25 16:56:32 -0300

    Update CorsConfig.java

--- library/src/main/java/sawaki/yuri/library/config/CorsConfig.java
+++ library/src/main/java/sawaki/yuri/library/config/CorsConfig.java
@@ -6,16 +6,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:63342/library/static/index.html?_ijt=p02a46s97it5jrj0kjlrivl64v&_ij_reload=RELOAD_ON_SAVE")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") 
+                .allowedOrigins("http://localhost:63342") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE") 
+                .allowedHeaders("*"); 
     }
 }
